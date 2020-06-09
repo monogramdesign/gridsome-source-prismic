b828961c5f8ce348fdfacbb1c4266ed8cb72a512
Claudio Christian Fofiu <claudio@monogram.io>
2020-06-09 17:29:58 -0400

    replaced with the original plugin

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 const Prismic = require('prismic-javascript')
 const { documentParser, capitalize } = require('./utils')
-const singlePages = ['home', 'about', 'page_not_found']
 
 class PrismicSource {
-  constructor(api, { prismic_token, prismic_url, collection_prefix = 'Prismic', html_serializer = null, link_resolver = null }) {
+  constructor(api, { prismic_token, prismic_url, collection_prefix = 'Prismic' }) {
     this.token = prismic_token
     this.url = prismic_url
     this.prefix = collection_prefix
     this.prismic = null
-    this.link_resolver = link_resolver
-    this.html_serializer = html_serializer
 
     api.loadSource(async ({ addCollection }) => {
       await this.initPrismic()
@@ -22,13 +19,14 @@ class PrismicSource {
   }
 
   async loadCollections(addCollection) {
-    const { results } = await this.prismic.query('', {pageSize: 1000})
+    // get all prismic docs
+    const { results } = await this.prismic.query('')
 
-    let documentTypes = results.map((page) => {
-      return singlePages.includes(page.type) ? 'singlePage' : page.type
-    })
-
+    // get all the types
+    let documentTypes = results.map(r => r.type)
     documentTypes = [...new Set(documentTypes)]
+
+    // create a gridsome collection for each type
     const collections = {}
     documentTypes.forEach((type) => {
       collections[type] = addCollection({
@@ -36,15 +34,13 @@ class PrismicSource {
       })
     })
 
+    // for each prismic doc, parse all the supported fields
+    // and add a gridsome node
     results.forEach((document) => {
-      const collection = collections[singlePages.includes(document.type) ? 'singlePage' : document.type]
-      const cleanredUpDoc = documentParser(
-        document,
-        this.link_resolver,
-        this.html_serializer
-      )
-      
-      collection.addNode(cleanredUpDoc)
+      const collection = collections[document.type]
+      const formattedObject = documentParser(document)
+
+      collection.addNode(formattedObject)
     })
   }
 }
